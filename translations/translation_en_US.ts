0192727b190da59446287e48ebebeef134a2a072
yuzg <yuzg@sunseer.co.jp>
2019-01-24 11:21:25 +0900

    solve startMeeting problem

---
+++ translations/translation_en_US.ts
@@ -0,0 +1,78 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>nextPresidentSpeech/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>では社長、スピーチお願いします。</source>
+            <comment>Text</comment>
+            <translation type="obsolete">では社長、スピーチお願いします。</translation>
+        </message>
+        <message>
+            <source>では社長、締めのスピーチお願いします。</source>
+            <comment>Text</comment>
+            <translation type="obsolete">では社長、締めのスピーチお願いします。</translation>
+        </message>
+        <message>
+            <location filename="nextPresidentSpeech/behavior.xar" line="0"/>
+            <source>では社長、締めのスピーチをお願いします。</source>
+            <comment>Text</comment>
+            <translation type="unfinished">では社長、締めのスピーチをお願いします。</translation>
+        </message>
+    </context>
+    <context>
+        <name>startmeeting/behavior.xar:/Get Localized Text</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+    </context>
+    <context>
+        <name>startmeeting/behavior.xar:/Get Localized Text (1)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+    </context>
+    <context>
+        <name>thanksLastSpeech/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>社長お願いします。</source>
+            <comment>Text</comment>
+            <translation type="obsolete">社長お願いします。</translation>
+        </message>
+        <message>
+            <source>社長っ,お願いします。</source>
+            <comment>Text</comment>
+            <translation type="obsolete">社長っ,お願いします。</translation>
+        </message>
+    </context>
+    <context>
+        <name>thanksLastSpeech/behavior.xar:/Say (1)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+    </context>
+    <context>
+        <name>thanksLastSpeech/behavior.xar:/Say Timeout</name>
+        <message>
+            <source>時間です、ご注意お願いします。</source>
+            <comment>Text</comment>
+            <translation type="obsolete">時間です、ご注意お願いします。</translation>
+        </message>
+    </context>
+</TS>
